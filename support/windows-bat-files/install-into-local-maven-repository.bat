7b901954bbbcf2c77377239137dd237b33488732
dtcubed <dtcubed@gmail.com>
2015-02-19 16:02:16 -0600

    Windows BAT file to add acukestf to local Maven repo.

---
+++ support/windows-bat-files/install-into-local-maven-repository.bat
@@ -0,0 +1,22 @@
+ECHO OFF
+
+SET FILE_TO_INSTALL=target\acukestf-1.0-SNAPSHOT.jar
+
+SET MAVEN_ARTIFACT_ID=acukestf
+
+SET MAVEN_GROUP_ID=com.github.dtcubed.acukestf
+
+SET MAVEN_PACKAGING=jar
+
+SET MAVEN_VERSION=1.0-SNAPSHOT
+
+ECHO "================================================"
+ECHO "Installing Framework Into Local Maven Repository"
+ECHO "================================================"
+ECHO ON
+mvn install:install-file -Dfile=%FILE_TO_INSTALL% -DgroupId=%MAVEN_GROUP_ID% -DartifactId=%MAVEN_ARTIFACT_ID% -Dversion=%MAVEN_VERSION% -Dpackaging=%MAVEN_PACKAGING%
+
+GOTO AROUND
+ECHO "Display Version Information for Maven"
+mvn -version
+:AROUND
