d9eeb15e543bb079a41f76f3e84e6ad22905b2a1
dtcubed <dtcubed@gmail.com>
2015-03-29 13:16:18 -0500

    sync

--- src/main/java/com/github/dtcubed/acukestf/Driver01.java
+++ src/main/java/com/github/dtcubed/acukestf/Driver01.java
@@ -1,7 +1,23 @@
 package com.github.dtcubed.acukestf;
 
-/**
- * Created by dworden on 3/29/2015.
- */
+
 public class Driver01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Driver01() - hello world");
+
+        if (args.length == 2) {
+
+            String testSuiteFile      = args[0];
+            String featureFileBaseDir = args[1];
+
+        } else {
+
+            String errMsg = "Expecting arguments <testSuiteFile> <featureFileBaseDir>";
+            System.err.println(errMsg);
+            System.exit(1);
+        }
+
+    }
 }
